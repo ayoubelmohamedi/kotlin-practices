ec8f3f817c3c75f86d38fd798657f1fee6e73051
ayoubelmohamedi <ayoubprog61@gmail.com>
2020-05-11 01:17:58 +0000

    more functions

---
+++ src/Functions.kt
@@ -0,0 +1,33 @@
+
+/*two types of function in kotlin
+
+-- Kotlin Standard Library Function
+-- User-defined functions
+
+
+ */
+fun main(){
+    var number = 5.5
+    //return square root of a number
+    val result = Math.sqrt(number)
+
+    println(addNumber(number,result))
+    var res = addNumber(5.3,4.1) + 1
+    println(res)
+    addMyName("Ayoub")
+    println(getName("Ayoub","Elmohamedi"))
+}
+
+fun addNumber(n1 : Double,n2 : Double) : Int {
+    val sum = n1 + n2
+    val sumInt = sum.toInt()
+    return sumInt
+}
+
+// the function doesn't return any value (return type is Unit).
+fun addMyName(myName : String){
+    println("my name is $myName")
+}
+//two arg type String and result type String, with = instead of curly braces {}
+//using this is fair too : fun getName(firstName: String, lastName: String) = "$firstName $lastName"
+fun getName(firstName: String, lastName: String): String = "$firstName $lastName"
