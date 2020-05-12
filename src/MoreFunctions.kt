7ac5964db46044c300b05916f2edae7ad3c76967
ayoubelmohamedi <ayoubprog61@gmail.com>
2020-05-12 01:14:39 +0000

    named args && default args

---
+++ src/MoreFunctions.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("without argument")
+    printName()
+
+    println("using argument")
+    printName("Ayoub", 20)
+
+    //Named arg, is used to specify which arg we want to set (like passing the second arg and left the first one as default)
+    println("Named arguments")
+    printName(age = 45)
+
+}
+
+//function with default args if nothing passed
+fun printName(name: String = "none", age: Int = 0) {
+    println("Name is $name, age = $age")
+}
+
