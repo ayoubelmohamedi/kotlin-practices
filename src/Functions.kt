2bcee09ff535842911c3fe0c2d8bda3e43c8e30e
ayoubelmohamedi <ayoubprog61@gmail.com>
2020-05-12 00:59:05 +0000

    infix functions

--- src/Functions.kt
+++ src/Functions.kt
@@ -15,6 +15,12 @@ fun main() {
     println(res)
     addMyName("Ayoub")
     println(getName("Ayoub", "Elmohamedi"))
+    val s = MyClass()
+    //infix function with()
+    s with "Karim"
+    //print pyramid structure using infix func.
+    val p = Structure()
+    p createPyramid 7
 
 }
 
@@ -34,7 +40,7 @@ fun addMyName(myName: String) {
 fun getName(firstName: String, lastName: String): String = "$firstName $lastName"
 
 //default functions useful in inheritance when overriding
-fun read(b: Array<Int>, off: Int = 0, len: Int = b.size) : Int {
+fun read(b: Array<Int>, off: Int = 0, len: Int = b.size): Int {
     var i = off
     var total = 0
     while (i < len) {
@@ -42,4 +48,31 @@ fun read(b: Array<Int>, off: Int = 0, len: Int = b.size) : Int {
         i++
     }
     return total
+}
+
+//Infix function like "or" & "and" instead of ".or(arg)" || ".and()"
+//it must be a member fun and contain only one parameter.
+class MyClass() {
+    infix fun with(name: String) {
+        println("you are friend of $name")
+    }
+}
+
+//another infix fun
+class Structure() {
+
+    infix fun createPyramid(rows: Int) {
+        var k = 0
+        for (i in 1..rows) {
+            k = 0
+            for (space in 1..rows - i) {
+                print("  ")
+            }
+            while (k != 2 * i - 1) {
+                print("* ")
+                ++k
+            }
+            println()
+        }
+    }
 }
