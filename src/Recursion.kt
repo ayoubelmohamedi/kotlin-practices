596acd67818cfffe967ec5a209554ec858aba862
ayoubelmohamedi <ayoubprog61@gmail.com>
2020-05-23 10:19:58 +0000

    comment to revursion section

--- src/Recursion.kt
+++ src/Recursion.kt
@@ -2,11 +2,20 @@
 // And tail recursive function.
 
 fun main() {
+    val number = 5
     val result = factorial(4)
     println("result = $result")
+    println("factorial of $number = ${tailFactorial(number)}")
 }
 
 fun factorial(n: Int): Long {
     println("number = $n")
     return if (n == 1) n.toLong() else n * factorial(n - 1)
+}
+
+// Factorial Using Tail Recursion
+//A recursive function is eligible for tail recursion if the function call to itself is the last operation it performs
+tailrec fun tailFactorial(n: Int, run : Int = 1): Long {
+    println("number = $n")
+    return if (n == 1) run.toLong() else tailFactorial(n - 1,run*n)
 }
